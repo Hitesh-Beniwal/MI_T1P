1c7993f947acbf4aa4283051f8346035c135acf7
Hitesh Beniwal <hitesh0001beniwal@gmail.com>
2025-03-05 09:27:45 +0530

    sensethinkact.ino

--- mi.ino
+++ mi.ino
@@ -1,23 +1,22 @@
-const int PIR_SENSOR_PIN = 2;  
-const int LED_PIN = 6;         
+const int motionSensorPin = 2;  // Motion sensor input pin
+const int ledPin = 13;          // Built-in LED output pin
 
 void setup() {
-    Serial.begin(9600);        
-    pinMode(PIR_SENSOR_PIN, INPUT);  
-    pinMode(LED_PIN, OUTPUT);        
-    Serial.println("PIR Motion Sensor Ready...");
+    pinMode(motionSensorPin, INPUT);  // Set sensor pin as input
+    pinMode(ledPin, OUTPUT);          // Set LED pin as output
+    Serial.begin(9600);               // Start serial communication
 }
 
 void loop() {
-    int motionDetected = digitalRead(PIR_SENSOR_PIN);  
+    int motionDetected = digitalRead(motionSensorPin);  // Read sensor value
 
     if (motionDetected == HIGH) {  
-        digitalWrite(LED_PIN, HIGH);  
-        Serial.println("Motion Detected! LED ON");
-    } else {
-        digitalWrite(LED_PIN, LOW);  
-        Serial.println("No Motion. LED OFF");
+        digitalWrite(ledPin, HIGH); 
+        Serial.println("Motion Detected! LED ON"); 
+    } else {  
+        digitalWrite(ledPin, LOW); // Turn LED OFF
+        Serial.println("No Motion. LED OFF"); 
     }
-
-    delay(500);  
+    
+    delay(500); // Small delay to prevent rapid flickering
 }
